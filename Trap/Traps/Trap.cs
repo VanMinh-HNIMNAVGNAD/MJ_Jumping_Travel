e3864b53fa9135221e9095f4021a4a78abc582f5
Minh <vanminh00052@gmail.com>
2026-03-14 21:54:42 +0700

    add tscn file

--- Trap/Traps/Trap.cs
+++ Trap/Traps/Trap.cs
@@ -3,21 +3,21 @@ using Godot;
 
 public partial class Trap : Area2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        Monitoring = true;
-        Monitorable = true;
-        SetCollisionMaskValue(1, true);
-        SetCollisionMaskValue(2, true);
-        this.BodyEntered += OnBodyEntered;
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Monitoring = true;
+		Monitorable = true;
+		SetCollisionMaskValue(1, true);
+		SetCollisionMaskValue(2, true);
+		this.BodyEntered += OnBodyEntered;
+	}
 
-    public void OnBodyEntered(Node2D body)
-    {
-        if (body is ITrapTarget trapTarget)
-        {
-            trapTarget.Die();
-        }
-    }
+	public void OnBodyEntered(Node2D body)
+	{
+		if (body is ITrapTarget trapTarget)
+		{
+			trapTarget.Die();
+		}
+	}
 }
